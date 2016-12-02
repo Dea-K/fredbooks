6270e8e2c16eeb740c619fd40fd90250c7b4a7ed
Dea-k <lee5250@fredonia.edu>
2016-12-01 19:07:57 -0500

    Now a user can login and the whole pages can hold the username

---
+++ authenticate.php
@@ -0,0 +1,25 @@
+<?php
+include "top.php";
+// authenticate.php
+
+$username = $_POST["username"];
+$password = $_POST["pw"];
+
+include "db.php";
+$connection = DB::CreateConnection();
+
+$sql = $connection->prepare(
+  "SELECT username, pw, id FROM User WHERE username = ?"
+);
+
+$sql->bind_param("s", $username);
+$sql->execute();
+$sql->bind_result($foundName, $foundPassword, $foundId);
+$sql->fetch();
+if($foundName == $username && $foundPassword == $password) {
+  $_SESSION["logedUserName"] = $foundName;
+  header("Location: index.php");
+} else {
+  header("Location: index.php");
+}
+ ?>
--- login.php
+++ login.php
@@ -1,11 +1,19 @@
-<div class="<?php echo $logedIn == 'loged' ? 'loged' : 'login-container' ?>">
+<?php
+  if(isset($_SESSION["logedUserName"])) {
+    $_SESSION["username"] = $_SESSION["logedUserName"];
+    $_SESSION["logedIn"] = 'loged';
+  } else {
+    $_SESSION["logedIn"] = 'not';
+  }
+?>
+<div class="<?php echo $_SESSION["logedIn"] == 'loged' ? 'loged' : 'login-container' ?>">
 <!-- <div class="login-container"> -->
-  <form class="login" action="<?php echo $page . '.php' ?>" method="post">
+  <form class="login" action="authenticate.php" method="post">
       <label for="username">Username:</label>
       <input type="text" name="username" placeholder="Enter username">
 
       <label for="pw">Password:</label>
       <input type="password" name="pw" placeholder="Enter password">
-    <button type="submit" name="login">Log in</button>
+      <button type="submit" name="login">Log in</button>
   </form>
 </div>
