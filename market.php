0fba2e7925a204771839fa2632a1bf7f16551529
Dea-k <lee5250@fredonia.edu>
2016-12-02 16:07:33 -0500

    basic drop down layout 70% done

--- market.php
+++ market.php
@@ -4,19 +4,37 @@
 <?php include "login.php" ?>
 
 <div class="market-container">
-    <table class="market-table" border="1">
-      <tr>
-        <td rowspan="2" colspan="6" class="book-image">IMAGE</td>
-        <td colspan="1">Title</td>
-        <td colspan="1">Class</td>
-        <td colspan="1">Price</td>
-      </tr>
-      <tr>
-        <td colspan="1">Usage</td>
-        <td colspan="1">Status</td>
-        <td colspan="1">Major</td>
-      </tr>
-    </table>
+    <div class="drop-market-container">
+      <table class="drop-market-btn" border="1">
+        <tr>
+          <td rowspan="2" colspan="6" class="book-image">IMAGE</td>
+          <td colspan="1">Title</td>
+          <td colspan="1">Class</td>
+          <td colspan="1">Price</td>
+        </tr>
+        <tr>
+          <td colspan="1">edition</td>
+          <td colspan="1">Status</td>
+          <td colspan="1">Major</td>
+        </tr>
+      </table>
+      <div class="drop-market-content">
+          <p>list1</p><p>list1</p>
+          <p>list2</p><p>list1</p>
+          <p>list3</p><p>list1</p>
+      </div>
+    </div>
+</div>
+
+
+
+<div class="dropdown">
+  <button class="dropbtn">DropDown</button>
+  <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+  </div>
 </div>
 
 <?php include "bottom.php" ?>
