54f53eee080e148ea1f55727d8b50ea1baed3503
Dea-k <lee5250@fredonia.edu>
2016-12-04 03:44:28 -0500

    Changed logedId variable to global [logedID]

--- register-submit.php
+++ register-submit.php
@@ -9,12 +9,12 @@ if(isset($_SESSION["logedUserName"]) &&
    }
 
 if(isset($_POST["register-book-button"])) {
-  $logeduserId = DB::UserIdByUsernameAndPassword($logedUN, $logedPW);
+  $_SESSION["logedID"] = DB::UserIdByUsernameAndPassword($logedUN, $logedPW);
   DB::CreateBook($_POST["book-title"],
                   $_POST["book-author"],
                   $_POST["book-isbn"],
                   $_POST["book-price"],
-                  $logeduserId);
+                  $_SESSION["logedID"]);
   header("Location: market.php");
 }
 else {
